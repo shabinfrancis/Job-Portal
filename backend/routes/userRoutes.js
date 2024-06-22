dc342a9286aa4e7178dff9dfc37d30039295b591
Shabin Francis <shabinfrancis25@gmail.com>
2024-06-22 18:06:58 +0530

    Added routes for job history

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {allUsers, singleUser, editUser, deleteUser} = require('../controllers/userController');
+const {allUsers, singleUser, editUser, deleteUser, createUserJobHistory} = require('../controllers/userController');
 const { isAuthenticated, isAdmin } = require('../middleware/auth');
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get('/allusers', isAuthenticated, isAdmin, allUsers);
 router.get('/user/:id', isAuthenticated, singleUser);
 router.put('/user/edit/:id', isAuthenticated, editUser);
 router.delete('/admin/user/delete/:id', isAuthenticated, deleteUser);
+router.post('/user/jobhistory', isAuthenticated, createUserJobHistory);
 
 module.exports = router;
